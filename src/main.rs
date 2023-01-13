551d596e3419f990d9cc050c80a2e1207bf2d839
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-13 23:55:02 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use error_chain::error_chain;
+use select::document::Document;
+use select::predicate::Name;
+
+error_chain! {
+      foreign_links {
+          ReqError(reqwest::Error);
+          IoError(std::io::Error);
+      }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
