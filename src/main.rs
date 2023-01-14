7fc77cad73e8496a5a56ddc3db6676359edaf75a
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-14 00:00:53 +0000

    get hrefs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use error_chain::error_chain;
 use select::document::Document;
 use select::predicate::Name;
+use tokio;
 
 error_chain! {
       foreign_links {
@@ -9,6 +10,20 @@ error_chain! {
       }
 }
 
-fn main() {
-    println!("Hello, world!");
+// async main function tokio
+#[tokio::main]
+async fn main() -> Result<()> {
+    let body = reqwest::get("https://www.rust-lang.org/")
+        .await?
+        .text()
+        .await?;
+
+    let document = Document::from(&body[..]);
+
+    // get all hrefs
+    for node in document.find(Name("a")) {
+        println!("{}", node.attr("href").unwrap());
+    }
+
+    Ok(())
 }
